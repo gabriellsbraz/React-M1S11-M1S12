48d20c2e09216de05ddfe0298e73db7b967819b1
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-08-13 20:58:12 -0300

    [M1S11] Ex 8 - Organização de pages [aboutMe]

--- m1s11-mini-projeto/src/pages/aboutMe/index.jsx
+++ m1s11-mini-projeto/src/pages/aboutMe/index.jsx
@@ -1,8 +1,13 @@
+import { AboutMeStyled } from "./styled"
+
 function AboutMe() {
     return (
-        <>
-            AboutMe
-        </>
+        <AboutMeStyled>
+            <p>
+                At the moment I don't have personal projects, since I don't work in the area.
+                I am currently working in another branch, studying computer science and taking a course sponsored by Senai, called DEVinHouse.
+            </p>
+        </AboutMeStyled>
     )
 }
  
